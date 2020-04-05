5a8634d1be9a1eeabab79c8bde18b3b9b828bf7a
Jia Yee <jiayee_koe@mymail.sutd.edu.sg>
2020-04-05 21:52:40 +0800

    added contract expiry

--- Smart Contract ex9.sol
+++ Smart Contract ex9.sol
@@ -1,4 +1,5 @@
 pragma solidity ^0.4.24;
+// pragma experimental ABIEncoderV2;
 
 contract CoinToss{
     
@@ -8,7 +9,7 @@ contract CoinToss{
         string role;
     }
     
-    uint256 betAmount;
+    uint betAmount;
     Player flipper;
     bool flipperExist = false;
     Player better;
@@ -16,11 +17,12 @@ contract CoinToss{
     
     Player[2] private players;  // only 2 players
     uint8 count = 0;
+    string winner = "";
+    uint256 betShelfLife;
 
     constructor() public {}
     
     function registerPlayers (uint guess) public payable {
-        emit showCount(count);
         require (msg.sender != address(0));
         require (msg.value > 0, "Nil betting amount not allowed.");
         require (count < 2, "No more than 2 players");
@@ -35,7 +37,8 @@ contract CoinToss{
             setBet(msg.value);
             
         } else if (!betterExist) {
-            
+            require(msg.value == betAmount, "Wrong betting amount");
+            betShelfLife = now + 3 hours;
             better = players[count];
             players[count].role = "better";
             betterExist = true;
@@ -46,14 +49,12 @@ contract CoinToss{
         
     }
     
-    function setBet(uint amount) public payable{
-        require (msg.sender == flipper.addr);
-        
+    function setBet(uint amount) private {
+        require(msg.sender == flipper.addr);
         betAmount = amount;
     }
     
-    function flipCoin () public payable {
-        require (msg.sender == flipper.addr);
+    function flipCoin () payable external onlyFlipper{
         require (better.addr != address(0), "A better is required");
         
         uint flip = flipper.number%2;
@@ -62,16 +63,38 @@ contract CoinToss{
         if (flip == bet) {
             // better wins
             better.addr.transfer(address(this).balance);
+            winner = "better wins";
         } else {
             // flipper wins
             flipper.addr.transfer(address(this).balance);
+            winner = "flipper wins";
         }
         
+        emit showWinner(winner);
         count = 0;
+        winner = "";
+        betShelfLife = 0;
         
     }
     
-    event showCount(uint8 c);
+    function withdrawBet() public payable{
+        require(now >= betShelfLife, "Cannot withdraw when contract has not expired");
+        require(msg.sender != flipper.addr);
+        
+        msg.sender.transfer(address(this).balance);
+    }
+    
+    event showWinner(string w);
+    
+    modifier onlyFlipper () {
+        require(msg.sender == flipper.addr);
+        _;
+    }
+    
+    modifier notExpired() {
+        require(now < betShelfLife);
+        _;
+    }
     
     // fallback function
     function() public payable {
